b28fc34c49721457ac1a6aff1744c9a92734fd93
Eric McKevitt <erictmckevitt@gmail.com>
2023-06-12 17:45:31 -0600

    Added basic terminal component with typing animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var $: any;  
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portfolio';
+  isJqueryWorking: any = "JQuery is not working :(";
+
+  ngOnInit()  
+  {  
+    $(document).ready(() => {  
+      this.isJqueryWorking = '✅ JQuery is working!';  
+    });  
+  }
 }
