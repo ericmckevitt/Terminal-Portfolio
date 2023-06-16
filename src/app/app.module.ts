521d1453a757c1ea0fb2920c4adb0a0b4471e985
Eric McKevitt <erictmckevitt@gmail.com>
2023-06-16 09:31:01 -0600

    Added basic navbar componenet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TerminalComponent } from './terminal/terminal.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TerminalComponent
+    TerminalComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
